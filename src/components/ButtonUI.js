16663d5d4ad8bea9c28c25643dc7ffa17e533f73
xav-ie <xruizify@gmail.com>
2020-08-11 22:27:39 -0400

    better panning; must fix zoom function

--- src/components/ButtonUI.js
+++ src/components/ButtonUI.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const ButtonUI = ({ svgFit, width, height, x = 0, y = 0, curve = 0, text = "", background = "black", fontColor = "white", onClick = () => alert("Please change default behavior.")}) => {
+const ButtonUI = ({ svgFit, width, height, x = 0, y = 0, curve = 0, text = "", background = "black", fontColor = "white", onClick = () => alert("Please change default behavior.") }) => {
     const style = {
         //TODO: Implement hover styling
         cursor: "pointer"
     }
-    
+    useEffect(() => {
+        // effect
+        return () => {
+            // cleanup
+        }
+    }, [svgFit]);
+
     return (
-        <>
-            <rect x={parseFloat(x)+-svgFit.startX+"%"} y={parseFloat(y)+-svgFit.startY+"%"} width={width} height={height} rx={curve} fill={background} />
-            <text x={`${svgFit.startX + parseFloat(x) + parseFloat(width) / 2}%`} y={`${svgFit.startY + parseFloat(y) + parseFloat(height) / 2}%`} fill={fontColor} dominantBaseline="middle" textAnchor="middle">{text}</text>))
-              
-            <rect style={style} x={parseFloat(x) + -svgFit.startX + "%"} y={parseFloat(y) + -svgFit.startY + "%"} width={width} height={height} fill="#FF00FF00" onClick={onClick} />
-        </>
+        <g className="ButtonUI" transform={`translate(${svgFit.startX} ${svgFit.startY})`}>
+            <rect x={x} y={y} width={width} height={height} rx={curve} fill={background} />
+            <text x={parseFloat(x) + parseFloat(width) / 2} y={parseFloat(y) + parseFloat(height) / 2} fill={fontColor} dominantBaseline="middle" textAnchor="middle">{text}</text>
+            <rect style={style} x={x} y={y} width={width} height={height} fill="#FF00FF00" onClick={onClick} />
+        </g>
     );
 }
 
--- src/components/GraphUI.js
+++ src/components/GraphUI.js
@@ -6,7 +6,7 @@ import NodeUI from "./NodeUI";
 const GraphUI = () => {
     const [graph, setGraph] = useState(new Graph());
     const zoomCoeff = .3;
-    const moveCoeff = 7;
+    const moveCoeff = 20;
     const defaultSVGFit = {
         startX: 0,
         startY: 0,
@@ -22,9 +22,6 @@ const GraphUI = () => {
         }
     });
 
-    const nodes = [{ x: 40, y: 200 }, { x: 400, y: 250 }];
-
-
     const style = {
         border: "3px solid black",
         width: "calc(100vw - 2em)",
@@ -39,19 +36,18 @@ const GraphUI = () => {
     const zoom = (amount) => {
         setSVGFit({ ...svgFit, width: svgFit.width * amount, height: svgFit.height * amount })
     }
-    const move = ({ x=0, y=0 }) => {
+    const move = ({ x = 0, y = 0 }) => {
         setSVGFit({ ...svgFit, startX: svgFit.startX + x, startY: svgFit.startY + y, })
     }
     return (
         <svg viewBox={`${svgFit.startX} ${svgFit.startY} ${svgFit.width} ${svgFit.height}`} style={style} xmlns="http://www.w3.org/2000/svg" width="90px" height="90px">
-            <ButtonUI svgFit={svgFit} width={"10%"} height={"5%"} x={"90%"} y={"0%"} background="#333" text="RUN" onClick={runButtonClicked} />
-            {/* {nodes.map(({ x, y }) => <NodeUI key={x + "" + y} x={x} y={y} text={x} />)} */}
-            <ButtonUI svgFit={svgFit} width={"5%"} height={"5%"} x={"95%"} y={"95%"} background="#333" text="" onClick={() => zoom(1 - zoomCoeff)} />
-            <ButtonUI svgFit={svgFit} width={"5%"} height={"5%"} x={"90%"} y={"95%"} background="#333" text="" onClick={() => zoom(1 + zoomCoeff)} />
-            <ButtonUI svgFit={svgFit} width={"5%"} height={"5%"} x={"95%"} y={"90%"} background="#333" text="" onClick={() => move({ x: moveCoeff })} />
-            <ButtonUI svgFit={svgFit} width={"5%"} height={"5%"} x={"90%"} y={"90%"} background="#333" text="" onClick={() => move({ x: -moveCoeff })} />
-            <ButtonUI svgFit={svgFit} width={"5%"} height={"5%"} x={"90%"} y={"85%"} background="#333" text="" onClick={() => move({ y: moveCoeff })} />
-            <ButtonUI svgFit={svgFit} width={"5%"} height={"5%"} x={"95%"} y={"85%"} background="#333" text="" onClick={() => move({ y: -moveCoeff })} />
+            <ButtonUI svgFit={svgFit} width="100" height="50" x="900" y="0" background="#333" text="RUN" onClick={runButtonClicked} />
+            <ButtonUI svgFit={svgFit} width="50" height="50" x="950" y="950" background="#333" text="+" zoomCoeff={zoomCoeff} onClick={() => zoom(1 - zoomCoeff)} />
+            <ButtonUI svgFit={svgFit} width="50" height="50" x="900" y="950" background="#333" text="-" zoomCoeff={zoomCoeff} onClick={() => zoom(1 + zoomCoeff)} />
+            <ButtonUI svgFit={svgFit} width="50" height="50" x="900" y="900" background="#333" text="<" onClick={() => move({ x: moveCoeff })} />
+            <ButtonUI svgFit={svgFit} width="50" height="50" x="950" y="900" background="#333" text=">" onClick={() => move({ x: -moveCoeff })} />
+            <ButtonUI svgFit={svgFit} width="50" height="50" x="900" y="850" background="#333" text="up" onClick={() => move({ y: moveCoeff })} />
+            <ButtonUI svgFit={svgFit} width="50" height="50" x="950" y="850" background="#333" text="down" onClick={() => move({ y: -moveCoeff })} />
 
             {graph.N.cities.map(city => <NodeUI key={city.cityNumber} x={city.coord.x} y={city.coord.y} text={city.cityNumber} />)}
         </svg>
